b1fe16087924097c412cbd332665e1fcc1d15cb8
salah-alhajj <contact@salahaldain.com>
2024-07-19 13:56:24 +0300

    v0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='your_package_name',
-    version='0.1.0',
+    version='0.0.7',
     author='Salah Aldain Alhajj',
     author_email='contact@salahaldain.com',
     packages=find_packages(where='src'),
@@ -14,7 +14,7 @@ setup(
     description='A brief description of your package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://your-package-url.com',  # Your package URL
+
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
