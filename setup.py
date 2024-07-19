02faa022373d95bc7c3e6645d820ca53791a0516
salah-alhajj <contact@salahaldain.com>
2024-07-19 13:37:18 +0300

    v0.0.7

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
-from build_tools.config import load_settings
+from src.cythonizer.build_tools  import load_settings
 
 settings = load_settings()
 
 setup(
     name=settings['project']['name'],
     version=settings['project']['version'],
+    author="Salah Aldain Alhajj",
+    author_email="contact@salahaldain.com",
     packages=find_packages(where=settings['build']['path']),
     package_dir={'': settings['build']['path']},
     install_requires=settings['project']['install_requires'],
     python_requires=settings['project']['python_requires'],
-    author=settings['project'].get('author', ''),
-    author_email=settings['project'].get('author_email', ''),
     description=settings['project'].get('description', ''),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
